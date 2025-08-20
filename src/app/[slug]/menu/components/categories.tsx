d5d90d3db1ad7c1e5482bb6878ae510370e062f7
alvarodev12 <alsouza23@outlook.com>
2025-08-19 23:37:05 -0300

    fix: Products reference to products in length check

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -73,7 +73,7 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
       <h3 className="px-5 font-semibold pt-2">{selectedCategory.name}</h3>
       <Products products={selectedCategory.products} />
 
-      {products.length > 0 && (
+      {Products.length > 0 && (
         <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
           <div>
             <p className="text-xs text-muted-foreground">Total dos pedidos</p>
